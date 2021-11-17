65c4534b40f9c4ffd10735619d01ae58e0f4347e
Abhishek Thakur <abhishek4@gmail.com>
2021-11-17 17:21:34 +0100

    add many params

---
+++ wtfml/fold_generator.py
@@ -0,0 +1,91 @@
+"""
+__author__: Abhishek Thakur
+"""
+
+from dataclasses import dataclass
+from typing import List
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+from sklearn import model_selection
+
+from .enums import ProblemType
+from .logger import logger
+
+
+@dataclass
+class FoldGenerator:
+    train_df: pd.DataFrame
+    targets: List[str]
+    problem_type: ProblemType
+    num_folds: int = 5
+    shuffle: bool = True
+    seed: int = 42
+
+    def _create_folds(self, train_df, problem_type):
+        if "kfold" in train_df.columns:
+            self.num_folds = len(np.unique(train_df["kfold"]))
+            logger.info("Using `kfold` for folds from training data")
+            return train_df
+
+        logger.info("Creating folds")
+        train_df["kfold"] = -1
+        if problem_type in (ProblemType.binary_classification, ProblemType.multi_class_classification):
+            y = train_df[self.targets].values
+            kf = model_selection.StratifiedKFold(
+                n_splits=self.num_folds,
+                shuffle=True,
+                random_state=self.seed,
+            )
+            for fold, (_, valid_indicies) in enumerate(kf.split(X=train_df, y=y)):
+                train_df.loc[valid_indicies, "kfold"] = fold
+
+        elif problem_type == ProblemType.single_column_regression:
+            y = train_df[self.targets].values
+            num_bins = int(np.floor(1 + np.log2(len(train_df))))
+            if num_bins > 10:
+                num_bins = 10
+            kf = model_selection.StratifiedKFold(
+                n_splits=self.num_folds,
+                shuffle=True,
+                random_state=self.seed,
+            )
+            train_df["bins"] = pd.cut(
+                train_df[self.targets].values.ravel(),
+                bins=num_bins,
+                labels=False,
+            )
+            for fold, (_, valid_indicies) in enumerate(kf.split(X=train_df, y=train_df.bins.values)):
+                train_df.loc[valid_indicies, "kfold"] = fold
+            train_df = train_df.drop("bins", axis=1)
+
+        elif problem_type == ProblemType.multi_column_regression:
+            y = train_df[self.targets].values
+            kf = model_selection.KFold(
+                n_splits=self.num_folds,
+                shuffle=True,
+                random_state=self.seed,
+            )
+            for fold, (_, valid_indicies) in enumerate(kf.split(X=train_df, y=y)):
+                train_df.loc[valid_indicies, "kfold"] = fold
+        # TODO: use iterstrat
+        elif problem_type == ProblemType.multi_label_classification:
+            y = train_df[self.targets].values
+            kf = model_selection.KFold(
+                n_splits=self.num_folds,
+                shuffle=True,
+                random_state=self.seed,
+            )
+            for fold, (_, valid_indicies) in enumerate(kf.split(X=train_df, y=y)):
+                train_df.loc[valid_indicies, "kfold"] = fold
+        else:
+            raise Exception("Problem type not supported")
+        return train_df
+
+    def generate(self):
+        train_df = self._create_folds(self.train_df, self.problem_type)
+        return train_df
+
+    def get_fold(self, fold):
+        return self.train_df[self.train_df["kfold"] == fold]
---
+++ wtfml/params.py
@@ -0,0 +1,268 @@
+from dataclasses import dataclass
+from typing import Union
+
+import xgboost as xgb
+from optuna import trial
+from sklearn.ensemble import ExtraTreesClassifier, ExtraTreesRegressor, RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import Lasso, LinearRegression, LogisticRegression, Ridge
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.svm import LinearSVC, LinearSVR
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from .enums import ProblemType
+
+
+class Params:
+    model: Union[
+        RandomForestClassifier,
+        RandomForestRegressor,
+        ExtraTreesClassifier,
+        ExtraTreesRegressor,
+        LinearSVC,
+        DecisionTreeClassifier,
+        DecisionTreeRegressor,
+        LinearSVR,
+        Lasso,
+        Ridge,
+        GaussianNB,
+        MultinomialNB,
+        LinearRegression,
+        LogisticRegression,
+        xgb.XGBClassifier,
+        xgb.XGBRegressor,
+    ]
+    problem_type: ProblemType
+
+    def _xgb_base_params(self):
+        params = {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-2, 0.25, log=True),
+            "reg_lambda": trial.suggest_float("reg_lambda", 1e-8, 100.0, log=True),
+            "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 100.0, log=True),
+            "subsample": trial.suggest_float("subsample", 0.1, 1.0),
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.1, 1.0),
+            "max_depth": trial.suggest_int("max_depth", 1, 9),
+            "early_stopping_rounds": trial.suggest_int("early_stopping_rounds", 100, 500),
+            "n_estimators": trial.suggest_categorical("n_estimators", [7000, 15000, 20000]),
+        }
+        return params
+
+    def _xgb_cpu_params(self):
+        params = self._xgb_base_params()
+        params["tree_method"] = trial.suggest_categorical("tree_method", ["exact", "approx", "hist"])
+        params["booster"] = trial.suggest_categorical("booster", ["gbtree", "gblinear"])
+        if params["booster"] == "gbtree":
+            params["gamma"] = trial.suggest_float("gamma", 1e-8, 1.0, log=True)
+            params["grow_policy"] = trial.suggest_categorical("grow_policy", ["depthwise", "lossguide"])
+
+        return params
+
+    def _xgb_gpu_params(self):
+        params = self._xgb_base_params()
+        params["tree_method"] = "gpu_hist"
+        params["gpu_id"] = 0
+        params["predictor"] = "gpu_predictor"
+        return params
+
+    def _rf_base_params(self):
+        params = {
+            "n_estimators": trial.suggest_int("n_estimators", 10, 10000),
+            "max_depth": trial.suggest_int("max_depth", 2, 15),
+            "max_features": trial.suggest_categorical("max_features", ["auto", "sqrt", "log2", None]),
+            "min_samples_split": trial.suggest_int("min_samples_split", 2, 20),
+            "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 20),
+            "bootstrap": trial.suggest_categorical("bootstrap", [True, False]),
+        }
+        return params
+
+    def _rf_classifier_params(self):
+        params = self._rf_base_params()
+        params["criterion"] = trial.suggest_categorical("criterion", ["gini", "entropy"])
+        return params
+
+    def _rf_regressor_params(self):
+        params = self._rf_base_params()
+        params["criterion"] = trial.suggest_categorical(
+            "criterion",
+            [
+                "squared_error",
+                "absolute_error",
+                "poisson",
+            ],
+        )
+        return params
+
+    def _et_base_params(self):
+        params = {
+            "n_estimators": trial.suggest_int("n_estimators", 10, 10000),
+            "max_depth": trial.suggest_int("max_depth", 2, 15),
+            "max_features": trial.suggest_categorical("max_features", ["auto", "sqrt", "log2", None]),
+            "min_samples_split": trial.suggest_int("min_samples_split", 2, 20),
+            "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 20),
+            "bootstrap": trial.suggest_categorical("bootstrap", [True, False]),
+            "criterion": trial.suggest_categorical("criterion", ["gini", "entropy"]),
+        }
+        return params
+
+    def _et_classifier_params(self):
+        params = self._et_base_params()
+        params["criterion"] = trial.suggest_categorical("criterion", ["gini", "entropy"])
+        return params
+
+    def _et_regressor_params(self):
+        params = self._et_base_params()
+        params["criterion"] = trial.suggest_categorical("criterion", ["squared_error", "absolute_error"])
+        return params
+
+    def _dt_base_params(self):
+        params = {
+            "max_depth": trial.suggest_int("max_depth", 1, 15),
+            "min_samples_split": trial.suggest_int("min_samples_split", 2, 20),
+            "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 20),
+            "max_features": trial.suggest_categorical("max_features", ["auto", "sqrt", "log2", None]),
+            "splitter": trial.suggest_categorical("splitter", ["best", "random"]),
+        }
+        return params
+
+    def _dt_classifier_params(self):
+        params = self._dt_base_params()
+        params["criterion"] = trial.suggest_categorical("criterion", ["gini", "entropy"])
+        return params
+
+    def _dt_regressor_params(self):
+        params = self._dt_base_params()
+        params["criterion"] = trial.suggest_categorical(
+            "criterion", ["squared_error", "absolute_error", "friedman_mse", "poisson"]
+        )
+        return params
+
+    def _linear_regression_params(self):
+        params = {
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+        }
+        return params
+
+    def _logistic_regression_params(self):
+        params = {
+            "C": trial.suggest_float("C", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "solver": trial.suggest_categorical("solver", ["liblinear", "saga", "newton-cg", "lbfgs"]),
+        }
+        if params["solver"] == "liblinear":
+            params["penalty"] = trial.suggest_categorical("penalty", ["l1", "l2"])
+        elif params["solver"] == "saga":
+            params["penalty"] = trial.suggest_categorical("penalty", ["l1", "l2", "elasticnet", "none"])
+        elif params["solver"] == "newton-cg":
+            params["penalty"] = trial.suggest_categorical("penalty", ["l2", "none"])
+        elif params["solver"] == "lbfgs":
+            params["penalty"] = trial.suggest_categorical("penalty", ["l2", "none"])
+        else:
+            raise ValueError("Unknown solver: {}".format(params["solver"]))
+        return params
+
+    def _linear_svc_params(self):
+        params = {
+            "C": trial.suggest_float("C", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "loss": trial.suggest_categorical("loss", ["hinge", "squared_hinge"]),
+            "penalty": trial.suggest_categorical("penalty", ["l1", "l2"]),
+            "max_iter": trial.suggest_int("max_iter", 1000, 10000),
+        }
+        return params
+
+    def _linear_svr_params(self):
+        params = {
+            "C": trial.suggest_float("C", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "loss": trial.suggest_categorical("loss", ["epsilon_insensitive", "squared_epsilon_insensitive"]),
+            "epsilon": trial.suggest_float("epsilon", 1e-8, 1e-1, log=True),
+            "max_iter": trial.suggest_int("max_iter", 1000, 10000),
+        }
+        return params
+
+    def _lasso_params(self):
+        params = {
+            "alpha": trial.suggest_float("alpha", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "max_iter": trial.suggest_int("max_iter", 1000, 10000),
+        }
+        return params
+
+    def _ridge_params(self):
+        params = {
+            "alpha": trial.suggest_float("alpha", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "max_iter": trial.suggest_int("max_iter", 1000, 10000),
+        }
+        return params
+
+    def _gaussian_nb_params(self):
+        params = {}
+        return params
+
+    def _multinomial_nb_params(self):
+        params = {}
+        return params
+
+    def _sgd_base_params(self):
+        params = {
+            "penalty": trial.suggest_categorical("penalty", ["l2", "l1", "elasticnet"]),
+            "alpha": trial.suggest_float("alpha", 1e-8, 1e3, log=True),
+            "fit_intercept": trial.suggest_categorical("fit_intercept", [True, False]),
+            "max_iter": trial.suggest_int("max_iter", 1000, 10000),
+            "tol": trial.suggest_float("tol", 1e-8, 1e-1, log=True),
+            "learning_rate": trial.suggest_categorical(
+                "learning_rate", ["constant", "optimal", "invscaling", "adaptive"]
+            ),
+            "eta0": trial.suggest_float("eta0", 1e-8, 1e3, log=True),
+            "power_t": trial.suggest_float("power_t", 1e-8, 1e3, log=True),
+            "early_stopping": trial.suggest_categorical("early_stopping", [True, False]),
+            "validation_fraction": trial.suggest_float("validation_fraction", 1e-8, 1e-1, log=True),
+            "n_iter_no_change": trial.suggest_int("n_iter_no_change", 1, 100),
+        }
+        return params
+
+    def _sgd_classifier_params(self):
+        params = self._sgd_base_params()
+        params["loss"] = trial.suggest_categorical(
+            "loss", ["hinge", "log", "modified_huber", "squared_hinge", "perceptron"]
+        )
+        return params
+
+    def _sgd_regressor_params(self):
+        params = self._sgd_base_params()
+        params["loss"] = trial.suggest_categorical("loss", ["squared_loss", "huber", "epsilon_insensitive"])
+        return params
+
+    def fetch_params(self):
+        if isinstance(self.model, RandomForestClassifier):
+            params = self._rf_classifier_params()
+        elif isinstance(self.model, RandomForestRegressor):
+            params = self._rf_regressor_params()
+        elif isinstance(self.model, ExtraTreesClassifier):
+            params = self._et_classifier_params()
+        elif isinstance(self.model, ExtraTreesRegressor):
+            params = self._et_regressor_params()
+        elif isinstance(self.model, DecisionTreeClassifier):
+            params = self._dt_classifier_params()
+        elif isinstance(self.model, DecisionTreeRegressor):
+            params = self._dt_regressor_params()
+        elif isinstance(self.model, LinearRegression):
+            params = self._linear_regression_params()
+        elif isinstance(self.model, LogisticRegression):
+            params = self._logistic_regression_params()
+        elif isinstance(self.model, LinearSVC):
+            params = self._linear_svc_params()
+        elif isinstance(self.model, LinearSVR):
+            params = self._linear_svr_params()
+        elif isinstance(self.model, Lasso):
+            params = self._lasso_params()
+        elif isinstance(self.model, Ridge):
+            params = self._ridge_params()
+        elif isinstance(self.model, GaussianNB):
+            params = self._gaussian_nb_params()
+        elif isinstance(self.model, MultinomialNB):
+            params = self._multinomial_nb_params()
+        else:
+            raise ValueError("Unknown model: {}".format(self.model))
+
+        return params
